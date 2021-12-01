81b203b364437e2809b11637b0c161ac367b622d
hpb-one <95324109+hpb-one@users.noreply.github.com>
2021-12-01 01:39:06 +0000

    Update LiquidityGeneratorToken.sol

--- LiquidityGeneratorToken.sol
+++ LiquidityGeneratorToken.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "https://github.com/hpb-one/contracts/blob/main/IUniswapV2Factory.sol";
-import "../interfaces/IUniswapV2Router02.sol";
+import "https://github.com/hpb-one/contracts/blob/main/IUniswapV2Router02.sol";
 import "../interfaces/IUniswapV2Pair.sol";
 import "../interfaces/ILiquidityGeneratorToken.sol";
 
